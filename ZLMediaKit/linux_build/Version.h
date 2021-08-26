89833dd153f1f680daadc26facab6cf1aa40ebcb
mass <mazengrong1211@163.com>
2021-08-26 16:55:13 +0800

    2022.8.26 Mass build fix

--- ZLMediaKit/linux_build/Version.h
+++ ZLMediaKit/linux_build/Version.h
@@ -1,9 +1,9 @@
 #ifndef __GIT_VERSION_H__
 #define __GIT_VERSION_H__
 
-#define COMMIT_HASH "1a1555e"
+#define COMMIT_HASH "5d7a84c"
 #define BRANCH_TIME "main"
-#define BUILD_TIME "2021/08/26-15:45:28"
+#define BUILD_TIME "2021/08/26-16:54:17"
 
 #endif //__GIT_VERSION_H__
 
