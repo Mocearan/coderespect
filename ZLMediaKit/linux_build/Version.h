6c9a6a777fdd238f82a7c8bae1740779e3ec1d20
mass <mazengrong1211@163.com>
2021-08-31 17:29:10 +0800

    2021.8.31 Mass flamingo init

--- ZLMediaKit/linux_build/Version.h
+++ ZLMediaKit/linux_build/Version.h
@@ -1,9 +1,9 @@
 #ifndef __GIT_VERSION_H__
 #define __GIT_VERSION_H__
 
-#define COMMIT_HASH "5d7a84c"
+#define COMMIT_HASH "89833dd"
 #define BRANCH_TIME "main"
-#define BUILD_TIME "2021/08/26-16:54:17"
+#define BUILD_TIME "2021/08/31-17:23:36"
 
 #endif //__GIT_VERSION_H__
 
